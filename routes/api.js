e41bbe2522452ebf8ee90ebd1c3df50b9d62a2fd
virtuallyawake <virtuallyawake@gmail.com>
2017-06-01 00:12:47 +0200

    Added a route to get all items for a user and a (partial) test.

--- routes/api.js
+++ routes/api.js
@@ -2,4 +2,15 @@ var express = require('express');
 var Item = require('../models/item');
 var router = express.Router();
 
+// GET all todo items for a user
+router.get('/:owner/items', function(req, res, next) {
+    var owner = req.params.owner;
+    Item.find({}, function(err, items) {
+	if (err)
+	    res.status(500).send(err);
+
+	res.json(items);
+    });
+});
+
 module.exports = router;
--- test/itemTest.js
+++ test/itemTest.js
@@ -20,4 +20,19 @@ describe('Item', function() {
         });     
     });
 
+    // Test GETting all items for a user
+    describe('Get all items for a user', function() {
+	it('should return an array of all the items for a particular user', function(done) {
+	    var owner = "daniela";
+	    chai.request(server)
+		.get('/api/' + owner + '/items')
+		.end(function(err, res) {
+		    console.dir(res.body);
+		    res.should.have.status(200);
+		    res.body.should.be.an('array');
+		    res.body.should.have.lengthOf(0);
+		    done();
+		});
+	});
+    });
 });
