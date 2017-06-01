bd9381cb84269b0eb8204a21b8123ab468af1ad9
virtuallyawake <virtuallyawake@gmail.com>
2017-06-01 22:55:21 +0200

    Added route to remove an item and 3 tests.

--- routes/api.js
+++ routes/api.js
@@ -28,9 +28,24 @@ router.get('/:owner/items', function(req, res, next) {
     var owner = req.params.owner;
     Item.find({}, function(err, items) {
 	if (err)
-	    res.status(500).send(err);
+	    return res.status(500).send(err);
 
 	res.json(items);
+    });
+});
+
+// Remove an item given an id.
+router.delete('/:owner/items/:item_id', function(req, res, next) {
+    var owner = req.params.owner;
+    var itemId = req.params.item_id;
+    Item.findByIdAndRemove(itemId, function(err, item) {
+	if (err)
+	    return res.status(500).send(err);
+
+	// If the db returns null (couldn't find the id), return an empty object
+	if (!item)
+	    item = {};
+	res.json(item);
     });
 });
 
